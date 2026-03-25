85c3b27f842ddad49cb651423fd617ff93d63b26
GrzegorzGurdak <grzesiul01@o2.pl>
2026-03-26 00:12:07 +0100

    added myVec

---
+++ src/MVec.cpp
@@ -0,0 +1,63 @@
+#include "MVec.hpp"
+
+#include <iostream>
+
+const Vec2 Vec2::nullV = Vec2();
+const Vec2 Vec2::UP = Vec2(0,1);
+const Vec2 Vec2::DOWN = Vec2(0,-1);
+const Vec2 Vec2::LEFT = Vec2(-1,0);
+const Vec2 Vec2::RIGHT = Vec2(1,0);
+
+std::ostream& operator<< (std::ostream& out, const Vec2& obj)
+{
+	return out << "(" << obj.x << ", " << obj.y << ")";
+}
+
+std::ostream& operator<< (std::ostream& out, const Vec2&& obj)
+{
+	return out << "(" << obj.x << ", " << obj.y << ")";
+}
+
+float Vec2::angleBetween(Vec2 base, Vec2 target)
+{
+	float det = (target.x * base.y - target.y * base.x);
+
+	float dot = (target.x * base.x + target.y * base.y);
+
+
+	//if(det>0 && dot > 0) return atan2f(det, dot) * 180 / M_PI;
+
+	//if(dot>0) return atan2f(det, dot) * 180 / M_PI; // det<0
+
+	//if (det < 0) return 360 + atan2f(det, dot);// *180.f / M_PIF; // det <0 -180
+
+	return atan2f(det, dot);// *180.f / M_PIF;
+}
+
+
+const Vec3 Vec3::nullV = Vec3();
+const Vec3 Vec3::UP = Vec3(0,1,0);
+const Vec3 Vec3::DOWN = Vec3(0,-1,0);
+const Vec3 Vec3::LEFT = Vec3(-1,0,0);
+const Vec3 Vec3::RIGHT = Vec3(1,0,0);
+const Vec3 Vec3::FORWARD = Vec3(0,0,1);
+const Vec3 Vec3::BACK = Vec3(0,0,-1);
+
+std::ostream& operator<< (std::ostream& out, const Vec3& obj)
+{
+	return out << "(" << obj.x << ", " << obj.y << ", " << obj.z << ")";
+}
+
+std::ostream& operator<< (std::ostream& out, const Vec3&& obj)
+{
+	return out << "(" << obj.x << ", " << obj.y << ", " << obj.z << ")";
+}
+
+//function to calculate angle between two 3D vectors
+float Vec3::angleBetween(Vec3 base, Vec3 target)
+{
+	float dot = base.x * target.x + base.y * target.y + base.z * target.z;
+	float det = base.x * target.y - base.y * target.x + base.x * target.z - base.z * target.x + base.y * target.z - base.z * target.y;
+
+	return atan2f(det, dot);
+}
---
+++ src/MVec.hpp
@@ -0,0 +1,111 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+
+#include <cmath>
+
+#ifndef M_PIF
+# define M_PIF	3.14159265358979323846f
+#endif
+
+
+struct Vec2
+{
+    Vec2(float _x = 0, float _y = 0) : x{ _x }, y{ _y } {};
+    Vec2(int _x, int _y) : x{ (float)_x}, y{ (float)_y} {};
+    //Vec2(int _x, int _y) : x{(float)_x}, y{(float)_y} {};
+    Vec2(sf::Vector2f _v) : x{ _v.x }, y{ _v.y } {};
+    Vec2(sf::Vector2u _v) : x{ (float)_v.x}, y{ (float)_v.y} {};
+    float x;
+    float y;
+
+    inline Vec2& set(float _x, float _y) { x = _x; y = _y; return *this; }
+    inline Vec2& set(int _x, int _y) { x = float(_x); y = float(_y); return *this; }
+
+    Vec2 operator+ (const Vec2 other) const { return Vec2(x + other.x, y + other.y); }
+    Vec2 operator- (const Vec2 other) const { return Vec2(x - other.x, y - other.y); }
+    Vec2 operator+= (const Vec2 other) { x += other.x; y += other.y; return *this; }
+    Vec2 operator-= (const Vec2 other) { x -= other.x; y -= other.y; return *this; }
+    Vec2 operator* (const Vec2 other) const { return Vec2(x * other.x, y * other.y); }
+    Vec2 operator*= (const Vec2 other) { x *= other.x; y *= other.y; return *this; }
+
+    bool operator== (const Vec2 other) const { return x == other.x && y == other.y; }
+    bool operator!= (const Vec2 other) const { return x != other.x || y != other.y; }
+
+    Vec2 operator* (const float _m) const { return Vec2(x * _m, y * _m); }
+    Vec2 operator/ (const float _m) const { return Vec2(x / _m, y / _m); }
+
+    operator sf::Vector2f() const { return sf::Vector2f(x, y); }
+
+    float length() const { return sqrt(x*x + y*y); };
+
+    static const Vec2 nullV;
+    static const Vec2 UP;
+    static const Vec2 DOWN;
+    static const Vec2 LEFT;
+    static const Vec2 RIGHT;
+
+    static Vec2 random_rad(float max_radius) {
+        float angle = rand() * 2.f * M_PIF / RAND_MAX;
+        return Vec2(sinf(angle), cosf(angle)) * (max_radius * rand() / RAND_MAX);
+    }
+
+    static float angleBetween(const Vec2 base, const Vec2 target);
+};
+
+std::ostream& operator<< (std::ostream& out, const Vec2& obj);
+
+std::ostream& operator<< (std::ostream& out, const Vec2&& obj);
+
+
+struct Vec3
+{
+    Vec3(float _x = 0, float _y = 0, float _z = 0) : x{ _x }, y{ _y }, z{ _z } {};
+    Vec3(int _x, int _y, int _z = 0) : x{ (float)_x }, y{ (float)_y }, z{ (float)_z } {};
+    //Vec2(int _x, int _y) : x{(float)_x}, y{(float)_y} {};
+    Vec3(sf::Vector3f _v) : x{ _v.x }, y{ _v.y }, z{ _v.z } {};
+    Vec3(sf::Vector3i _v) : x{ (float)_v.x}, y{ (float)_v.y}, z{ (float)_v.z} {};
+    float x;
+    float y;
+    float z;
+
+    inline Vec3& set(float _x, float _y, float _z) { x = _x; y = _y; z = _z; return *this; }
+    inline Vec3& set(int _x, int _y, int _z) { x = (float)_x; y = (float)_y; z = (float)_z; return *this; }
+
+    Vec3 operator+ (const Vec3 other) const { return Vec3(x + other.x, y + other.y, z + other.z); }
+    Vec3 operator- (const Vec3 other) const { return Vec3(x - other.x, y - other.y, z - other.z); }
+    Vec3 operator+= (const Vec3 other) { x += other.x; y += other.y; z += other.z; return *this; }
+    Vec3 operator-= (const Vec3 other) { x -= other.x; y -= other.y; z -= other.z; return *this; }
+    Vec3 operator* (const Vec3 other) const { return Vec3(x * other.x, y * other.y, z * other.z); }
+    Vec3 operator*= (const Vec3 other) { x *= other.x; y *= other.y; z *= other.z; return *this; }
+
+    bool operator== (const Vec3 other) const { return x == other.x && y == other.y && z == other.z; }
+    bool operator!= (const Vec3 other) const { return x != other.x || y != other.y || z != other.z; }
+
+    Vec3 operator* (const float _m) const { return Vec3(x * _m, y * _m, z * _m); }
+    Vec3 operator/ (const float _m) const { return Vec3(x / _m, y / _m, z / _m); }
+
+    operator sf::Vector3f() const { return sf::Vector3f(x, y, z);  }
+
+    float length() const { return sqrt(x*x + y*y + z*z); };
+
+    static const Vec3 nullV;
+    static const Vec3 UP;
+    static const Vec3 DOWN;
+    static const Vec3 LEFT;
+    static const Vec3 RIGHT;
+    static const Vec3 FORWARD;
+    static const Vec3 BACK;
+
+    static Vec3 random_rad(float max_radius) {
+        float yaw = rand() * 2.f * M_PIF / RAND_MAX;
+        float pitch = rand() * 2.f * M_PIF / RAND_MAX;
+        return Vec3(sinf(yaw) * cosf(pitch), sinf(pitch), cosf(yaw) * cosf(pitch)) * (max_radius * rand() / RAND_MAX);
+    }
+
+    static float angleBetween(const Vec3 base, const Vec3 target);
+};
+
+std::ostream& operator<< (std::ostream& out, const Vec3& obj);
+
+std::ostream& operator<< (std::ostream& out, const Vec3&& obj);
