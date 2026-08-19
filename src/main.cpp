b1f3d2e043d342e16e2d97bdf5d4999df040ef38
mujes523 <mujes523@gmail.com>
2026-08-20 01:37:14 +0200

    mose control orbit camera

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,7 @@ int main(int argc, char** argv)
     std::cout << "SFML 3 + OpenGL demo" << std::endl;
     std::cout << "Vec2 a: " << a << std::endl;
 
-    OpenGLGraphics oglGraphics(900, 1.5f, 0);
+    OpenGLGraphics oglGraphics(400.f, 1.5f, 0.f);
 
     sf::RenderWindow window(sf::VideoMode({1200, 900}), "SFML 3 + CMake");
     window.setFramerateLimit(60);
@@ -30,7 +30,13 @@ int main(int argc, char** argv)
 	oglGraphics.reshapeScreen(window.getSize());
     oglGraphics.initOpenGL(argc, argv);
 
-    float angle = 0.f;
+    bool orbiting = false;
+    sf::Vector2i lastMousePos;
+    constexpr float orbitSensitivity = 0.005f;
+    // Kept just under +-pi/2: at exactly the pole the up vector
+    // (0, R*cos(phi), 0) in OpenGLGraphics::setCamera degenerates to zero,
+    // which flips the view.
+    constexpr float phiLimit = 1.5f;
 
     Planet ball(Vec3(0.f, 0.f, 0.f), 50.f, 5, true);
 
@@ -42,7 +48,7 @@ int main(int argc, char** argv)
 
     StatElement statElement(font);
 
-    PhysicSolver3d sandbox(ChunkGrid3d(20.f, 1.f, Vec3(-200.f, -200.f, -200.f), Vec3(200.f, 200.f, 200.f)), &ball);
+    PhysicSolver3d sandbox(ChunkGrid3d(6.f, 2.f, Vec3(-200.f, -200.f, -200.f), Vec3(200.f, 200.f, 200.f)), &ball);
 	PhysicDrawer3d sandbox_draw(sandbox);
 
     // acceleration function: to the center of Vec3(0,0,0)
@@ -62,12 +68,14 @@ int main(int argc, char** argv)
     for(int i = 0; i < 5000; i++)
     {
         PhysicBody3d* obj = new PhysicBody3d(
-            Vec3::random_rad(80.f, 100.f),
-            1.f,
+            Vec3::random_rad(90.f, 100.f),
+            2.f,
             sf::Color(rand() % 256, rand() % 256, rand() % 256)
             //ocean blue sf::Color(0, 105, 148)
             //sf::Color(11, 57, 135)
         );
+        //move slightly towards the center to avoid stalling
+        obj->setPos(obj->getPos() + (obj->getPos().normal() * -0.001f));
         sandbox.add(obj);
         // controlObj = obj; //last added object will be for debug
     }
@@ -91,17 +99,53 @@ int main(int argc, char** argv)
                 else if (keyPressed->scancode == sf::Keyboard::Scancode::F)
                     ball.toggleFilled();
             }
+            else if (const auto* mousePressed = event->getIf<sf::Event::MouseButtonPressed>())
+            {
+                if (mousePressed->button == sf::Mouse::Button::Right)
+                {
+                    orbiting = true;
+                    lastMousePos = mousePressed->position;
+                }
+            }
+            else if (const auto* mouseReleased = event->getIf<sf::Event::MouseButtonReleased>())
+            {
+                if (mouseReleased->button == sf::Mouse::Button::Right)
+                    orbiting = false;
+            }
+            else if (const auto* mouseMoved = event->getIf<sf::Event::MouseMoved>())
+            {
+                if (orbiting)
+                {
+                    const sf::Vector2i delta = mouseMoved->position - lastMousePos;
+                    lastMousePos = mouseMoved->position;
+
+                    oglGraphics.theta += delta.x * orbitSensitivity;
+                    oglGraphics.phi -= delta.y * orbitSensitivity;
+                    if (oglGraphics.phi > phiLimit) oglGraphics.phi = phiLimit;
+                    if (oglGraphics.phi < -phiLimit) oglGraphics.phi = -phiLimit;
+                }
+            }
         }
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        glMatrixMode(GL_MODELVIEW);
-        glLoadIdentity();
-        glTranslatef(0.f, 0.f, -400.f);
-        glRotatef(angle, 0.f, 1.f, 0.f);
+        oglGraphics.setCamera();
 
-        sandbox.update(timeResults, 1 / 30.f, 8);
+        sandbox.update(timeResults, 1 / 30.f, 4);
         statElement.update();
+
+
+        std::cout << "Time results (us): "
+            << "acceleration: " << timeResults[0] << ", "
+            << "constraints: " << timeResults[1] << ", "
+            << "links: " << timeResults[2] << ", "
+            << "assign grid: " << timeResults[3] << ", "
+            << "collision: " << timeResults[4] << ", "
+            << "position: " << timeResults[5] << ", "
+            << "planet collision: " << timeResults[6] << "\n";
+
+        for (int i = 0; i < 7; i++)
+            timeResults[i] = 0;
 
         //std::cout << controlObj->getPos() << "\n";
 
@@ -111,10 +155,6 @@ int main(int argc, char** argv)
         window.pushGLStates();
 		window.draw(statElement);
 		window.popGLStates();
-
-        angle += 0.005f * 50;
-        if (angle >= 360.f)
-            angle -= 360.f;
 
         window.display();
     }
